64d98d83e0bee85a7540c31bdb5d95cf494bc486
Jordan Carter <jordan.d.carter@gmail.com>
2011-05-24 00:24:01 +1200

    first commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+
+gem 'pg'
+gem 'nifty-generators', :group => :development
+gem 'simple_form'
+gem 'sass'
+
+group :development, :test do
+  gem "unicorn"
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'rspec-rails'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'wirble'
+  gem 'silent-postgres'
+  gem 'capistrano'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lipo::Application.config.secret_token = '9977f544a1fa7e9dad2a715372305840790e83d0fc2413389c9ae596cd997a9156b694a95f0edb253d4449e06a9e18bf50a832480aaee905ded5bdc22fca3353'
